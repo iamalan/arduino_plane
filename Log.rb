325974216b9f213b0caff6fcdb35b03340db0671
Iain Blair <iain.blair.mail@gmail.com>
2012-02-08 17:14:48 +1030

    Created singleton log able to write to screen or file

--- Log.rb
+++ Log.rb
@@ -1,20 +1,44 @@
+require 'singleton'
+
 class Log
-  def initialize(file_name)
-    @this_log = File.open(file_name, 'w')
-    add("Log opened.")
-  end
+  include Singleton
   
+  @@file = true
+ 
   def add(line)
-    @this_log.puts("#{Time.now} #{Time.now.usec} #{line}")
+    if @@file
+      @this_log.puts("#{Time.now} #{Time.now.usec} #{line}")
+    else
+      puts("#{Time.now} #{Time.now.usec} #{line}")
+    end
   end
   
   def close
     add('Closing log.')
-    @this_log.close
+    if @@file
+      @this_log.close
+    end
   end
+  
+  # call these before anything else!
+  def log_to_screen
+    @@file = false
+    add('Screen log started.')
+  end
+  
+  def log_to_file
+    @@file = true
+    @this_log = File.open('log.txt', 'w')
+    add('File log opened.')
+  end
+  
 end
 
-# # Example usage.
-# log = Log.new 'log.txt'
-# log.add 'Did something.'
-# log.close
+# # Example usage for log to file
+# Log.instance.log_to_file
+# Log.instance.add 'hey'
+# Log.instance.close
+# 
+# # Example usage for log to screen
+# Log.instance.log_to_screen
+# Log.instance.add 'hey'
