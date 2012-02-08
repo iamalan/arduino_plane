d8ace3a427356c9e79e172730244425e5bc28eee
Iain Blair <iain.blair.mail@gmail.com>
2012-02-08 18:23:08 +1030

    Add sendArray function with some max byte checking

--- Arduino.rb
+++ Arduino.rb
@@ -3,13 +3,13 @@ require 'YAML'
 require 'serialport'
 
 require 'log'
+require 'valuemapper'
 
 class Arduino
   def initialize(config_file)   
     begin
       @CONFIG = File.open(config_file) { |f| YAML.load(f) }
     
-      Log.instance.log_to_screen
       Log.instance.add "Opened Arduino config #{config_file}:"
       Log.instance.add @CONFIG.to_yaml
     
@@ -19,7 +19,27 @@ class Arduino
         Log.instance.add "#{e} #{e.backtrace}"
       end   
   end
+  
+  def sendArray(array)
+    begin
+      Log.instance.add "#{self.class.name} #{__method__} called with #{array.inspect}."
+      @sp.write([255].pack('C'));
+      Log.instance.add "Wrote sync byte over serial."
+      checksum = 0;
+      array.each do |value|
+        @sp.write([value].pack('C'))
+        Log.instance.add "Wrote #{value} over serial."
+        if value > ValueMapper::BYTE_MAX_VALUE then Log.instance.add "Warning, last value written was over BYTE_MAX_VALUE of #{ValueMapper::BYTE_MAX_VALUE}." end
+        checksum = checksum + value
+      end
+      @sp.write([checksum].pack('s'))
+      Log.instance.add "Wrote #{checksum} over serial."
+    rescue Exception => e
+        Log.instance.add "#{e} #{e.backtrace}"
+    end
+  end
 end
 
 a = Arduino.new 'arduino_config.yaml'
+a.sendArray([300,255,255])
 
