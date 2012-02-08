75e45efd7e71b173221f4f8510e41880080f2d2d
Iain Blair <iain.blair.mail@gmail.com>
2012-02-08 16:16:42 +1030

    Add class to log events with timestamps

---
+++ Log.rb
@@ -0,0 +1,21 @@
+class Log
+  def initialize(file_name)
+    @this_log = File.open(file_name, 'w')
+    add("Log opened.")
+  end
+  
+  def add(line)
+    @this_log.puts("#{Time.now} #{Time.now.usec} #{line}")
+  end
+  
+  def close
+    add('Closing log.')
+    @this_log.close
+  end
+end
+
+
+# Example usage.
+log = Log.new 'log.txt'
+log.add 'Did something'
+log.close
