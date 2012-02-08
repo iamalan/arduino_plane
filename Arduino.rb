966a552101210946ea9544b730ebf20986511463
Iain Blair <iain.blair.mail@gmail.com>
2012-02-08 17:16:00 +1030

    Create Arduino controller class

---
+++ Arduino.rb
@@ -0,0 +1,29 @@
+require 'rubygems'
+require 'YAML'
+require 'serialport'
+
+require 'log'
+
+class Arduino
+  def initialize(config_file)   
+    begin
+    
+      @CONFIG = File.open(config_file) { |f| YAML.load(f) }
+    
+      Log.instance.log_to_screen
+      Log.instance.add "Opened Arduino config #{config_file}:"
+      Log.instance.add @CONFIG.to_yaml
+    
+      @sp = SerialPort.new(@CONFIG["serial"], @CONFIG["baud"], @CONFIG["data_bits"], @CONFIG["stop_bits"], SerialPort::NONE)
+    
+      rescue => msg
+        Log.instance.add msg
+      end   
+  end
+end
+
+
+
+
+a = Arduino.new 'arduino_config.yaml'
+
